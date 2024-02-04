0d7f86a151bd54dbc48fefebee6496e9f83b641f
Risheit Munshi <risheit.munshi@gmail.com>
2024-02-03 23:29:41 -0500

    Add google cloud and kotlin dependencies

--- legalquestionizer/build.gradle.kts
+++ legalquestionizer/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+  kotlin("jvm") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "io.shaded"
@@ -16,7 +18,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
   implementation(platform("org.jdbi:jdbi3-bom:3.44.0"))
@@ -25,6 +30,8 @@ dependencies {
   implementation("org.jdbi:jdbi3-postgres")
   implementation("org.postgresql:postgresql:42.7.1")
   implementation("com.zaxxer:HikariCP:5.1.0")
+
+  implementation("com.google.cloud:google-cloud-vertexai:0.1.0")
 }
 
 tasks.withType<Test> {
